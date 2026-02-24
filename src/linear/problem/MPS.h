41c1ffc9fbf09ce05a00d43859f1a25daa02bf2b
MishkaSimakov <msimakov661@gmail.com>
2026-02-24 11:01:43 +0300

    updated MPS reader: - integer variables via MARKER - space separated columns parsing

--- src/linear/problem/MPS.h
+++ src/linear/problem/MPS.h
@@ -12,6 +12,8 @@
 #include "linear/matrix/Matrix.h"
 #include "utils/String.h"
 
+enum class MPSFieldsMode { FIXED_WIDTH, SPACE_SEPARATED };
+
 template <typename Field>
 class MPSReader {
   enum class SectionType {
@@ -41,27 +43,45 @@ class MPSReader {
   struct Bounds {
     std::optional<Field> lower = 0;
     std::optional<Field> upper = std::nullopt;
+
+    bool is_integer = false;
   };
 
-  static std::array<std::string, 6> get_parts(const std::string& str) {
+  std::array<std::string, 6> get_parts(const std::string& str) const {
     constexpr size_t kNumFields = 6;
     constexpr size_t kFieldStartPos[kNumFields] = {1, 4, 14, 24, 39, 49};
     constexpr size_t kFieldLength[kNumFields] = {2, 8, 8, 12, 8, 12};
 
     std::array<std::string, 6> result;
 
-    for (size_t i = 0; i < kNumFields; ++i) {
-      size_t start = kFieldStartPos[i];
-      size_t length = kFieldLength[i];
+    if (mode_ == MPSFieldsMode::FIXED_WIDTH) {
+      for (size_t i = 0; i < kNumFields; ++i) {
+        size_t start = kFieldStartPos[i];
+        size_t length = kFieldLength[i];
 
-      if (start >= str.size()) {
-        break;
-      }
-      if (start + length > str.size()) {
-        length = std::string::npos;
-      }
+        if (start >= str.size()) {
+          break;
+        }
+        if (start + length > str.size()) {
+          length = std::string::npos;
+        }
 
-      result[i] = str::rtrim(str.substr(start, length));
+        result[i] = str::rtrim(str.substr(start, length));
+      }
+    } else if (mode_ == MPSFieldsMode::SPACE_SEPARATED) {
+      result[0] = str::rtrim(str.substr(kFieldStartPos[0], kFieldLength[0]));
+
+      size_t current_index = 1;
+      for (size_t i = kFieldStartPos[1]; i < str.size(); ++i) {
+        if (i > 0 && std::isspace(str[i]) != 0 &&
+            std::isspace(str[i - 1]) == 0) {
+          ++current_index;
+        } else if (std::isspace(str[i]) == 0) {
+          result[current_index] += str[i];
+        }
+      }
+    } else {
+      throw std::runtime_error("Unknown field mode in MPSReader.");
     }
 
     return result;
@@ -130,8 +150,16 @@ class MPSReader {
     } else if (type == "PL") {
       bounds.lower = 0;
       bounds.upper = std::nullopt;
+    } else if (type == "LI") {
+      bounds.is_integer = true;
+      bounds.lower = value;
+      bounds.upper = std::nullopt;
+    } else if (type == "UI") {
+      bounds.is_integer = true;
+      bounds.lower = std::nullopt;
+      bounds.upper = value;
     } else {
-      throw std::runtime_error("Unsupported bound type.");
+      throw std::runtime_error(std::format("Unsupported bound type: {}", type));
     }
   }
 
@@ -147,11 +175,26 @@ class MPSReader {
     return false;
   }
 
+  std::optional<std::string> get_marker_type(
+      const std::array<std::string, 6>& parts) {
+    if (parts[2] != "'MARKER'") {
+      return std::nullopt;
+    }
+
+    return parts[3];
+  }
+
+  MPSFieldsMode mode_;
+
   ObjectiveType objective_ = ObjectiveType::MINIMIZE;
   std::unordered_map<std::string, Row> rows_;
   std::unordered_map<std::string, Bounds> bounds_;
 
+  bool is_integer_section_ = false;
+
  public:
+  explicit MPSReader(MPSFieldsMode mode) : mode_(mode) {}
+
   void read(const std::filesystem::path& filepath) {
     std::ifstream is(filepath);
     std::string line;
@@ -184,18 +227,29 @@ class MPSReader {
       if (current_section == SectionType::ROWS) {
         rows_.emplace(parts[1], Row(decode_row_type(parts[0])));
       } else if (current_section == SectionType::COLUMNS) {
-        std::string variable_name = parts[1];
-        bounds_.emplace(variable_name, Bounds{});
+        auto marker = get_marker_type(parts);
 
-        for (size_t i = 2; i < parts.size(); i += 2) {
-          std::string row_name = parts[i];
-
-          if (row_name.empty()) {
-            break;
+        if (marker.has_value()) {
+          if (marker == "'INTORG'") {
+            is_integer_section_ = true;
+          } else if (marker == "'INTEND") {
+            is_integer_section_ = false;
           }
+        } else {
+          std::string variable_name = parts[1];
+          auto [itr, _] = bounds_.emplace(variable_name, Bounds{});
+          itr->second.is_integer = is_integer_section_;
 
-          rows_.at(row_name).variables.emplace_back(variable_name,
-                                                    parse_field(parts[i + 1]));
+          for (size_t i = 2; i < parts.size(); i += 2) {
+            std::string row_name = parts[i];
+
+            if (row_name.empty()) {
+              break;
+            }
+
+            rows_.at(row_name).variables.emplace_back(
+                variable_name, parse_field(parts[i + 1]));
+          }
         }
       } else if (current_section == SectionType::RHS) {
         // rhs vector name is ignored
@@ -235,16 +289,22 @@ class MPSReader {
   // generates a problem suitable for simplex method:
   // c x -> max, s.t. Ax = b, l <= x <= u
   MILPProblem<Field> get_canonical_representation() {
+    const Field kInfinity = 1e7;
+
     MILPProblem<Field> result;
     std::unordered_map<std::string, Variable<Field>> variables;
 
     for (const Row& row : rows_ | std::views::values) {
       for (const std::string& name : row.variables | std::views::keys) {
         if (!variables.contains(name)) {
-          auto lower = bounds_.at(name).lower;
-          auto upper = bounds_.at(name).upper;
+          const Bounds& bound = bounds_.at(name);
 
-          auto var = result.new_variable(name, VariableType::REAL, lower ? *lower : -1e7, upper ? *upper : 1e7);
+          auto variable_type =
+              bound.is_integer ? VariableType::INTEGER : VariableType::REAL;
+
+          auto var = result.new_variable(name, variable_type,
+                                         bound.lower.value_or(-kInfinity),
+                                         bound.upper.value_or(kInfinity));
           variables.emplace(name, var);
         }
       }
